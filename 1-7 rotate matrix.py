3ac6c8b5f971b1f1eb097755c9e4462e0e144d84
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-08 19:47:41 +0500

    uncomplete code

--- 1-7 rotate matrix.py
+++ 1-7 rotate matrix.py
@@ -1,1 +1,7 @@
-def anyway(m):
+def solution1(M, rotate):
+    # it's a simple brute force solution where we copy each colum and add it to the new matrix
+    # and we keep doing that till we have done all columns
+
+
+    ans = []
+    for i in range(range(M[0]))

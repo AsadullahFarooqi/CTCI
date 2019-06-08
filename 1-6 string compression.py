0a78edb46562ac3d2ae5faaf0724d3e92abbca52
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-08 19:00:07 +0500

    commented the 6th problem of 1st chapter

--- 1-6 string compression.py
+++ 1-6 string compression.py
@@ -1,15 +1,22 @@
 def string_compression(s):
     ans = ""
-    temp = 0
-    for i in range(len(s)-1):
-        if s[i]  == s[i+1]:
+    temp = 0  # temporary variable that holds the number of times the character appears
+    # the loop will iterate throw the charachters till the 2nd last one
+    for i in range(len(s) - 1):
+        # this if checks if the current charachter is equal to the next one
+        if s[i] == s[i + 1]:
+            # if yes add one to temp and continue
             temp += 1
             continue
-        ans += s[i]+str(temp+1)
+    
+        # when current charachter is not equal to the next then it'll merge it with ans and the temp count
+        ans += s[i] + str(temp + 1)
         temp = 1
+    # this line handles the last charachter since the temp has already stored the count of last character
     ans += s[-1] + str(temp)
     return ans
 
+
 if __name__ == "__main__":
-    n = "aabcccccaaa"
+    n = "aabcccccaaad"
     print(string_compression(n))

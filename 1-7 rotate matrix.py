5cd6bbaad76f90631af6170d9d8b8dbaacc5bce2
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-09 13:48:33 +0500

    1st solution done 2nd is uncomplete

--- 1-7 rotate matrix.py
+++ 1-7 rotate matrix.py
@@ -1,7 +1,27 @@
-def solution1(M, rotate):
+def solution1(M, rotate=None):
     # it's a simple brute force solution where we copy each colum and add it to the new matrix
     # and we keep doing that till we have done all columns
 
+    args = (0, len(M[0]), 1)
+
+    if rotate == "A" or rotate == "a":
+        args = (len(M[0])-1, -1, -1)
 
     ans = []
-    for i in range(range(M[0]))
+    for col in range(*args):
+        temp = [M[row][col] for row in range(len(M)-1,-1,-1)]
+        ans.append(temp)
+    return ans
+
+def solution2():
+    # NxN matrix inplace rotation
+    
+
+if __name__ == '__main__':
+    m = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+    ]
+
+    print(solution1(m, "a"))

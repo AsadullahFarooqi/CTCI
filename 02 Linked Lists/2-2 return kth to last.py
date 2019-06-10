592d748660c0e73334979aa163bb641eee46e4b6
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-11 00:55:15 +0500

    2nd problem of 2nd chapter added ideas are ready need to code kth from the last

---
+++ 02 Linked Lists/2-2 return kth to last.py
@@ -0,0 +1,19 @@
+def solution1(root, kth):
+    # finding the length of the linked list and then returning
+    # the kth element it'll take O(2N)
+
+    pass
+
+def recursive(root, kth, k):
+    # Get to end of the linked list and recurlivly keep track of the elements
+    # and return 
+
+    pass
+
+def iteratively(root, kth):
+    # iterate on the list with two pointers with the kth difference by the same pace 
+    # so when the 2nd pointer hits the end the first one will be on kth 
+    pass
+
+if __name__ == '__main__':
+    main()

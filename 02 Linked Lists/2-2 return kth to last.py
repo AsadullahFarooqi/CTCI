66ce609bafc8a77a77c4c5358273b7ec9841379b
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-15 23:45:47 +0500

    all the solution are tested and ready to go

--- 02 Linked Lists/2-2 return kth to last.py
+++ 02 Linked Lists/2-2 return kth to last.py
@@ -1,3 +1,23 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.next = None
+
+def create_list(l):
+    r = Node(l[0])
+    temp = r
+    for i in l[1:]:
+        t = Node(i)
+        temp.next = t
+        temp = t
+    return r
+
+def print_l(r):
+    while r:
+        print(r.val)
+        r = r.next
+    return
+
 def solution1(root, kth):
     # finding the length of the linked list and then returning
     # the kth element it'll take O(2N)
@@ -19,43 +39,32 @@ def recursive(root,k):
 
     if not root.next:
         return root.val,1
-    
     s,kth = recursive(root.next,k)
-        if kth == k:
-            return s
+    if kth == k:
+        return s, kth
     
-    return root.val, kth+1
+    return root, kth+1
 
 
 def iteratively(root, kth):
     # iterate on the list with two pointers with the kth difference by the same pace 
     # so when the 2nd pointer hits the end the first one will be on kth 
     
-    i = 0
-    pass
+    i = 1
+    temp = root
+    while i <= kth:
+        temp = temp.next
+        i += 1
 
-class Node:
-    def __init__(self, value):
-        self.val = value
-        self.next = None
-
-def create_list(l):
-    r = Node(l[0])
-    temp = r
-    for i in l[1:]:
-        t = Node(i)
-        temp.next = t
-        temp = t
-    return r
-
-def print_l(r):
-    while r:
-        print(r.val)
-        r = r.next
-    return
+    k = root
+    while temp:
+        temp = temp.next
+        k = k.next
+    return k
 
 
 if __name__ == '__main__':
     l = [23, 53, 66, 99, 90, 49, 12]
     r = create_list(l)
-    print(recursive(r, 2))
+    # print(recursive(r, 2)[0].val)
+    print(iteratively(r, 2).val)

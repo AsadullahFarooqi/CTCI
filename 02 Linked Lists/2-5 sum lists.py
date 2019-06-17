2f647a0bb33f5bf1641447bf2974cd8f8a94dfe6
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-17 21:32:58 +0500

    documentation added for 2-5

--- 02 Linked Lists/2-5 sum lists.py
+++ 02 Linked Lists/2-5 sum lists.py
@@ -1,6 +1,25 @@
+"""Summary
+"""
 from LinkedList import LinkedList
 
 def solution_iterative(A,B):
+    """
+    Steps that this algorithm takes
+    1 - iterate through list A and appends each node value of the 
+        list to string type var a 
+    2 - iterate through list B and appends each node value of the 
+        list to string type var b 
+    3 - convert the a,b to ints and add the results store in c
+    4 - call the LinkedList function create_list to create a new 
+        list from the digits of c and return root of the list
+    Args:
+        A (Linked List): first number each node is a digit
+        B (Linked List): second number each node is a digit
+    
+    Returns:
+        Linked List: result of the sum converted to linked list
+            and each digit is a node
+    """
     a = ""
     while A:
         a += str(A.val)
@@ -12,7 +31,7 @@ def solution_iterative(A,B):
         b += str(B.val)
         B = B.next
 
-    c = str(int(a) + int(b))
+    c = [int(i) for i in str(int(a) + int(b))]
 
     r = LinkedList().create_list(c)
     return r

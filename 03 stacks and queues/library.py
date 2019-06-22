132d02591ee1b947a1a455489135c6b0399c8c34
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2019-06-22 14:57:33 +0500

    stack and queue library added but need a little more work

---
+++ 03 stacks and queues/library.py
@@ -0,0 +1,39 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, v):
+        self.stack.append(v)
+        return True
+    
+    def pop(self, v):
+        if self.is_empty():
+            return False
+        return self.pop()
+
+    def is_empty(self):
+        return bool(self.stack)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.stack[-1]
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+
+    def add(self, item):
+        if self.is_empty():
+            return False
+        self.queue = [item] + self.queue
+        return True
+
+    def remove(self):
+        if self.is_empty():
+            return False
+        self.queue = self.queue[1:]
+        return True
+
+    def peek(self ):
